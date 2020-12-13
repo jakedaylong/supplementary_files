36194d0e4347e79984c68f8fc0b79c755f2c869c
Luis Conejo-Alpizar <luisconej@gmail.com>
2020-12-12 19:59:49 -0800

    Updated integrate_main.py

--- src/py220_supplemental/lessons/lesson07/integrate_exercise/integrate.py
+++ src/py220_supplemental/lessons/lesson07/integrate_exercise/integrate.py
@@ -1,3 +1,5 @@
+import numpy
+
 def f(x):
     return x**2
 
@@ -7,3 +9,12 @@ def integrate(f, a, b, N):
     for i in range(N):
         s += f(a+i*dx)
     return s * dx
+
+def integrate_numpy(f, a, b, N):
+    '''
+    Source: https://helloacm.com/how-to-compute-numerical-integration-in-numpy-python/
+    '''
+    x = numpy.linspace(a+(b-a)/(2*N), b-(b-a)/(2*N), N)
+    fx = f(x)
+    area = numpy.sum(fx)*(b-a)/N
+    return area
