f975c162a287bde4183cde5b3f37b1f718561e3b
Andy Miles <akmiles@icloud.com>
2021-01-18 07:59:21 -0800

    revised gitignore

--- src/py220_supplemental/lessons/lesson01/extras/snoop.py
+++ src/py220_supplemental/lessons/lesson01/extras/snoop.py
@@ -2,11 +2,12 @@ import pysnooper
 import math
 
 
-#@pysnooper.snoop(depth=3)
+@pysnooper.snoop(depth=3)
 def messy_function(x: int, y: int, z: int) -> int:
     x = y + z
     n = math.sqrt(x)
     y = n / 10
     return 7
 
-messy_function(9,3,7)
+
+messy_function(9, 3, 7)
